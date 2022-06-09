92fc393d12fafd6875721a25e4b22c87798c3474
shawjitesh <jiteshshaw93@gmail.com>
2022-06-09 18:21:48 +0530

    Show userid and password on welcome page - ModelMap and @RequestParam

--- spring-boot-first-web-application/src/main/java/com/example/springboot/web/springbootfirstwebapplication/controller/LoginController.java
+++ spring-boot-first-web-application/src/main/java/com/example/springboot/web/springbootfirstwebapplication/controller/LoginController.java
@@ -1,16 +1,22 @@
 package com.example.springboot.web.springbootfirstwebapplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
+	
+	
 
 	@RequestMapping("/login")
 //	@ResponseBody
-	public String loginMessage() {
+	public String loginMessage(@RequestParam String name, ModelMap model) {
 //		return "Hello World!";
+		model.put("name", name);
+		System.out.println("name is: " + name);
 		return "login";
 	}
 }
