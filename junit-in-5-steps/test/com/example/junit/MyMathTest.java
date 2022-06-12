f0d0b9bff3e31982bf024930616d35ef595fb5e0
shawjitesh <jiteshshaw93@gmail.com>
2022-06-12 19:02:09 +0530

    Important annotations

--- junit-in-5-steps/test/com/example/junit/MyMathTest.java
+++ junit-in-5-steps/test/com/example/junit/MyMathTest.java
@@ -2,19 +2,45 @@ package com.example.junit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
 	
 	MyMath myMath = new MyMath();
+	
+	@BeforeEach
+	public void before() {
+		System.out.println("Before");
+	}
+	
+	@AfterEach
+	public void after() {
+		System.out.println("After");
+	}
+	
+	@BeforeAll
+	public static void beforeClass() {
+		System.out.println("Before All");
+	}
+	
+	@AfterAll
+	public static void afterClass() {
+		System.out.println("After All");
+	}
 
 	@Test
 	public void sumWith3Numbers() {
+		System.out.println("Test1");
 		assertEquals(6, myMath.sum(new int[] {1, 2, 3}));
 	}
 	
 	@Test
-	public void sumWith1Number() {		
+	public void sumWith1Number() {	
+		System.out.println("Test2");
 		assertEquals(3, myMath.sum(new int[] {3}));
 	}
 }
