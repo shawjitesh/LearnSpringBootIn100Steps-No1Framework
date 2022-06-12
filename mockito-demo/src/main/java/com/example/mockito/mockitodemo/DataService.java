b50bf7a4153ec7a9b166349421c180a9a167d7b5
shawjitesh <jiteshshaw93@gmail.com>
2022-06-12 19:12:59 +0530

    Setting up an example using http://start.spring.io

---
+++ mockito-demo/src/main/java/com/example/mockito/mockitodemo/DataService.java
@@ -0,0 +1,5 @@
+package com.example.mockito.mockitodemo;
+
+interface DataService {
+	int[] retrieveAllData();
+}
---
+++ mockito-demo/src/main/java/com/example/mockito/mockitodemo/SomeBusinessImpl.java
@@ -0,0 +1,17 @@
+package com.example.mockito.mockitodemo;
+
+public class SomeBusinessImpl {
+
+	private DataService dataService;
+	
+	int findTheGreatestFromAllData() {
+		
+		int[] data = dataService.retrieveAllData();
+		int greatest = Integer.MIN_VALUE;
+		for(int value : data) {
+			if(value > greatest) greatest = value;
+		}
+		
+		return greatest;
+	}
+}
