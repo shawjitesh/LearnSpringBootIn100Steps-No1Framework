a9ce3a8dce138ae12adaf2c475e24efc2832ab7a
shawjitesh <jiteshshaw93@gmail.com>
2022-06-12 20:41:09 +0530

    Setup and Launch Spring Boot Application with Maven and Eclipse

---
+++ first-springboot-project/first-springboot-project/src/main/java/com/example/springboot/firstspringbootproject/FirstSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.firstspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ first-springboot-project/first-springboot-project/src/test/java/com/example/springboot/firstspringbootproject/FirstSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.firstspringbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
