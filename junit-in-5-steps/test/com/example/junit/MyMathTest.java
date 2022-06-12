3cf3bf491caecf42f1cbf2a99b8de8adad503ca0
shawjitesh <jiteshshaw93@gmail.com>
2022-06-12 18:46:39 +0530

    First Code and First Unit Test

--- junit-in-5-steps/test/com/example/junit/MyMathTest.java
+++ junit-in-5-steps/test/com/example/junit/MyMathTest.java
@@ -5,10 +5,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
+	
+	MyMath myMath = new MyMath();
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void sumWith3Numbers() {
+		assertEquals(6, myMath.sum(new int[] {1, 2, 3}));
 	}
-
+	
+	@Test
+	public void sumWith1Number() {		
+		assertEquals(3, myMath.sum(new int[] {3}));
+	}
 }
