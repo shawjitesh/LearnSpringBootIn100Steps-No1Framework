9624c34b52f862d4ad162f396f161d1207cb40a7
shawjitesh <jiteshshaw93@gmail.com>
2022-06-13 14:02:39 +0530

    Creating REST Service with @GetMapping and @PathVariable

---
+++ first-springboot-project/first-springboot-project/src/main/java/com/example/springboot/firstspringbootproject/controller/SurveyController.java
@@ -0,0 +1,23 @@
+package com.example.springboot.firstspringbootproject.controller;
+
+import java.util.List;
+
+import com.example.springboot.firstspringbootproject.model.Question;
+import com.example.springboot.firstspringbootproject.service.SurveyService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SurveyController {
+	
+	@Autowired
+	private SurveyService surveyService;
+
+	@GetMapping("/surveys/{surveyId}/questions")
+	public List<Question> retrieveQuestionsForSurvey(@PathVariable String surveyId) {
+		return surveyService.retrieveQuestions(surveyId);
+	}
+}
